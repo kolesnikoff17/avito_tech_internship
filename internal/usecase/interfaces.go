0836a324cbc85d707c6609a586d13aa050352a29
kolesnikoff17 <vladkolesnikofff@gmail.com>
2022-10-21 14:42:17 +0300

    added interfaces for model and repository

---
+++ internal/usecase/interfaces.go
@@ -0,0 +1,28 @@
+package usecase
+
+import (
+  "avito_tech_internship/internal/entity"
+  "context"
+)
+
+// Balance is an interface for model layer
+type Balance interface {
+  GetByID(ctx context.Context, id int) (entity.Balance, error)
+  CreateOrder(ctx context.Context, order entity.Order) error
+  CommitOrder(ctx context.Context, order entity.Order) error
+  RollbackOrder(ctx context.Context, order entity.Order) error
+  Increase(ctx context.Context, balance entity.Balance) error
+  GetHistory(ctx context.Context, history entity.History) (entity.History, error)
+  UpdateReport(ctx context.Context, year, month int) (string, error)
+}
+
+// BalanceRepo is an interface for repository layer
+type BalanceRepo interface {
+  GetByID(ctx context.Context, id int) (entity.Balance, error)
+  CreateOrder(ctx context.Context, order entity.Order) error
+  VerifyOrder(ctx context.Context, order entity.Order) error
+  ChangeOrderStatus(ctx context.Context, order entity.Order) error
+  Increase(ctx context.Context, balance entity.Balance) error
+  GetHistory(ctx context.Context, history entity.History) (entity.History, error)
+  GetReport(ctx context.Context, year, month int) (entity.Report, error)
+}
