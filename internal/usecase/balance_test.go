06312e6efc991e668a9f9b75d78c1ffbb86d5b8b
kolesnikoff17 <vladkolesnikofff@gmail.com>
2022-10-21 18:41:55 +0300

    added tests for model
    trying to follow tdd

---
+++ internal/usecase/balance_test.go
@@ -0,0 +1,269 @@
+package usecase
+
+import (
+  "balance_api/internal/entity"
+  reportmock "balance_api/internal/mocks/report"
+  repomock "balance_api/internal/mocks/repository"
+  "context"
+  "github.com/stretchr/testify/assert"
+  "testing"
+  "time"
+)
+
+func TestGetByID(t *testing.T) {
+  ctx := context.Background()
+  r := repomock.NewBalanceRepo(t)
+  uc := New(r, reportmock.NewReportFile(t))
+
+  r.On("GetByID", ctx, 1).Return(entity.Balance{ID: 1, Amount: "200"}, nil)
+  r.On("GetByID", ctx, 2).Return(entity.Balance{}, entity.ErrNoID)
+
+  type TestCase struct {
+    name        string
+    id          int
+    expectedVal entity.Balance
+    expectedErr error
+  }
+
+  cases := []TestCase{{
+    name: "valid",
+    id:   1,
+    expectedVal: entity.Balance{
+      ID:     1,
+      Amount: "200",
+    },
+    expectedErr: nil,
+  }, {
+    name:        "no such id",
+    id:          2,
+    expectedVal: entity.Balance{},
+    expectedErr: entity.ErrNoID,
+  },
+  }
+
+  for _, tc := range cases {
+    val, err := uc.GetByID(ctx, tc.id)
+    assert.Equal(t, tc.expectedVal, val)
+    assert.Equal(t, tc.expectedErr, err)
+  }
+}
+
+func TestCreateOrder(t *testing.T) {
+  ctx := context.Background()
+  r := repomock.NewBalanceRepo(t)
+  uc := New(r, reportmock.NewReportFile(t))
+
+  r.On("GetByID", ctx, 1).Return(entity.Balance{ID: 1, Amount: "300"}, nil)
+  r.On("CreateOrder", ctx, entity.Order{ID: 1, ServiceID: 1, UserID: 1, Sum: "200"}).
+    Return(nil)
+
+  r.On("GetByID", ctx, 2).Return(entity.Balance{ID: 2, Amount: "300"}, nil)
+  r.On("CreateOrder", ctx, entity.Order{ID: 2, ServiceID: 2, UserID: 2, Sum: "200"}).
+    Return(entity.ErrOrderExists)
+
+  r.On("GetByID", ctx, 3).Return(entity.Balance{}, entity.ErrNoID)
+
+  r.On("GetByID", ctx, 4).Return(entity.Balance{ID: 4, Amount: "100"}, nil)
+
+  type TestCase struct {
+    name        string
+    val         entity.Order
+    expectedErr error
+  }
+
+  cases := []TestCase{{
+    name:        "valid",
+    val:         entity.Order{ID: 1, ServiceID: 1, UserID: 1, Sum: "200"},
+    expectedErr: nil,
+  }, {
+    name:        "already exist",
+    val:         entity.Order{ID: 2, ServiceID: 2, UserID: 2, Sum: "200"},
+    expectedErr: entity.ErrOrderExists,
+  }, {
+    name:        "no such user",
+    val:         entity.Order{ID: 3, ServiceID: 3, UserID: 3, Sum: "200"},
+    expectedErr: entity.ErrNoID,
+  }, {
+    name:        "not enough money",
+    val:         entity.Order{ID: 4, ServiceID: 4, UserID: 4, Sum: "200"},
+    expectedErr: entity.ErrNotEnoughMoney,
+  },
+  }
+
+  for _, tc := range cases {
+    err := uc.CreateOrder(ctx, tc.val)
+    assert.Equal(t, tc.expectedErr, err)
+  }
+}
+
+func TestChangeOrderStatus(t *testing.T) {
+  ctx := context.Background()
+  r := repomock.NewBalanceRepo(t)
+  uc := New(r, reportmock.NewReportFile(t))
+
+  r.On("GetOrderByID", ctx, 1).
+    Return(entity.Order{ID: 1, ServiceID: 1, UserID: 1, Sum: "200", StatusID: 1}, nil)
+  r.On("CommitOrder", ctx, entity.Order{ID: 1, ServiceID: 1, UserID: 1, Sum: "200", StatusID: 2}).
+    Return(nil)
+
+  r.On("GetOrderByID", ctx, 2).
+    Return(entity.Order{ID: 2, ServiceID: 2, UserID: 2, Sum: "200", StatusID: 1}, nil)
+  r.On("RollbackOrder", ctx, entity.Order{ID: 2, ServiceID: 2, UserID: 2, Sum: "200", StatusID: 3}).
+    Return(nil)
+
+  r.On("GetOrderByID", ctx, 3).
+    Return(entity.Order{}, entity.ErrOrderNoExists)
+  r.On("GetOrderByID", ctx, 4).
+    Return(entity.Order{ID: 4, ServiceID: 4, UserID: 4, Sum: "200", StatusID: 1}, nil)
+  r.On("GetOrderByID", ctx, 4).
+    Return(entity.Order{ID: 5, ServiceID: 5, UserID: 5, Sum: "200", StatusID: 2}, nil)
+
+  type TestCase struct {
+    name        string
+    val         entity.Order
+    expectedErr error
+  }
+
+  cases := []TestCase{{
+    name:        "valid",
+    val:         entity.Order{ID: 1, ServiceID: 1, UserID: 1, Sum: "200", StatusID: 2},
+    expectedErr: nil,
+  }, {
+    name:        "valid rollback",
+    val:         entity.Order{ID: 2, ServiceID: 2, UserID: 2, Sum: "200", StatusID: 3},
+    expectedErr: nil,
+  }, {
+    name:        "no such order id",
+    val:         entity.Order{ID: 3, ServiceID: 3, UserID: 3, Sum: "200", StatusID: 2},
+    expectedErr: entity.ErrOrderNoExists,
+  }, {
+    name:        "wrong order data",
+    val:         entity.Order{ID: 4, ServiceID: 4, UserID: 4, Sum: "300", StatusID: 3},
+    expectedErr: entity.ErrOrderMismatch,
+  }, {
+    name:        "order already committed",
+    val:         entity.Order{ID: 5, ServiceID: 5, UserID: 5, Sum: "200", StatusID: 2},
+    expectedErr: entity.ErrCantChangeStatus,
+  },
+  }
+
+  for _, tc := range cases {
+    err := uc.ChangeOrderStatus(ctx, tc.val)
+    assert.Equal(t, tc.expectedErr, err)
+  }
+}
+
+func TestIncrease(t *testing.T) {
+  ctx := context.Background()
+  r := repomock.NewBalanceRepo(t)
+  uc := New(r, reportmock.NewReportFile(t))
+
+  r.On("GetByID", ctx, 1).Return(entity.Balance{}, entity.ErrNoID)
+  r.On("GetByID", ctx, 2).Return(entity.Balance{ID: 2, Amount: "1"}, nil)
+
+  type TestCase struct {
+    name        string
+    val         entity.Balance
+    expectedErr error
+  }
+
+  cases := []TestCase{{
+    name:        "valid new user",
+    val:         entity.Balance{ID: 1, Amount: "200"},
+    expectedErr: nil,
+  }, {
+    name:        "valid",
+    val:         entity.Balance{ID: 2, Amount: "200"},
+    expectedErr: nil,
+  },
+  }
+
+  for _, tc := range cases {
+    err := uc.Increase(ctx, tc.val)
+    assert.Equal(t, tc.expectedErr, err)
+  }
+}
+
+func TestGetHistory(t *testing.T) {
+  ctx := context.Background()
+  r := repomock.NewBalanceRepo(t)
+  uc := New(r, reportmock.NewReportFile(t))
+
+  r.On("GetByID", ctx, 1).Return(entity.Balance{ID: 1, Amount: "200"}, nil)
+  r.On("GetHistory", ctx, entity.History{UserID: 1, Limit: 10, OrderBy: "date", Asc: true, Cursor: ""}).
+    Return(entity.History{Orders: []entity.Order{
+      {ID: 1, ServiceName: "aboba", Status: "approved", Sum: "10", Time: time.Unix(1, 0)},
+      {ID: 2, ServiceName: "aboba", Status: "canceled", Sum: "20", Time: time.Unix(10, 0)},
+    }, UserID: 1, Limit: 10, OrderBy: "date", Asc: true, Cursor: ""}, nil)
+
+  r.On("GetByID", ctx, 2).Return(entity.Balance{}, entity.ErrNoID)
+
+  type TestCase struct {
+    name        string
+    val         entity.History
+    expectedVal entity.History
+    expectedErr error
+  }
+
+  cases := []TestCase{{
+    name: "valid",
+    val:  entity.History{UserID: 1, Limit: 10, OrderBy: "date", Asc: true, Cursor: ""},
+    expectedVal: entity.History{Orders: []entity.Order{
+      {ID: 1, ServiceName: "aboba", Status: "approved", Sum: "10", Time: time.Unix(1, 0)},
+      {ID: 2, ServiceName: "aboba", Status: "canceled", Sum: "20", Time: time.Unix(10, 0)},
+    }, UserID: 1, Limit: 10, OrderBy: "date", Asc: true, Cursor: ""},
+    expectedErr: nil,
+  }, {
+    name:        "no such user",
+    val:         entity.History{UserID: 2, Limit: 2, OrderBy: "sum", Asc: false, Cursor: ""},
+    expectedVal: entity.History{},
+    expectedErr: entity.ErrNoID,
+  },
+  }
+
+  for _, tc := range cases {
+    history, err := uc.GetHistory(ctx, tc.val)
+    assert.Equal(t, tc.expectedVal, history)
+    assert.Equal(t, tc.expectedErr, err)
+  }
+}
+
+func TestGetReport(t *testing.T) {
+  ctx := context.Background()
+  r := repomock.NewBalanceRepo(t)
+  f := reportmock.NewReportFile(t)
+  uc := New(r, f)
+
+  r.On("GetReport", ctx, 2022, 10).
+    Return(entity.Report{Sums: []entity.SumByService{{Sum: "1", Name: "a"}}}, nil)
+  f.On("Create", ctx, "2022-10", entity.Report{Sums: []entity.SumByService{{Sum: "1", Name: "a"}}}).
+    Return("2022-06.csv", nil)
+
+  r.On("GetReport", ctx, 1980, 1).Return(entity.Report{Sums: nil})
+
+  type TestCase struct {
+    name        string
+    date        []int
+    expectedVal string
+    expectedErr error
+  }
+
+  cases := []TestCase{{
+    name:        "valid",
+    date:        []int{2022, 10},
+    expectedVal: "2000-10.csv",
+    expectedErr: nil,
+  }, {
+    name:        "empty report",
+    date:        []int{1980, 1},
+    expectedVal: "",
+    expectedErr: entity.ErrEmptyReport,
+  },
+  }
+
+  for _, tc := range cases {
+    name, err := uc.UpdateReport(ctx, tc.date[0], tc.date[1])
+    assert.Equal(t, tc.expectedVal, name)
+    assert.Equal(t, tc.expectedErr, err)
+  }
+}
