2461342194c38a9410ffc28e34ba3813c72c0ee0
kolesnikoff17 <vladkolesnikofff@gmail.com>
2022-10-21 18:38:15 +0300

    updated interfaces
    added ReportFile interface for report files
    refactored old interfaces

--- internal/usecase/interfaces.go
+++ internal/usecase/interfaces.go
@@ -9,8 +9,7 @@ import (
 type Balance interface {
   GetByID(ctx context.Context, id int) (entity.Balance, error)
   CreateOrder(ctx context.Context, order entity.Order) error
-  CommitOrder(ctx context.Context, order entity.Order) error
-  RollbackOrder(ctx context.Context, order entity.Order) error
+  ChangeOrderStatus(ctx context.Context, order entity.Order) error
   Increase(ctx context.Context, balance entity.Balance) error
   GetHistory(ctx context.Context, history entity.History) (entity.History, error)
   UpdateReport(ctx context.Context, year, month int) (string, error)
@@ -20,9 +19,16 @@ type Balance interface {
 type BalanceRepo interface {
   GetByID(ctx context.Context, id int) (entity.Balance, error)
   CreateOrder(ctx context.Context, order entity.Order) error
-  VerifyOrder(ctx context.Context, order entity.Order) error
-  ChangeOrderStatus(ctx context.Context, order entity.Order) error
+  GetOrderByID(ctx context.Context, id int) (entity.Order, error)
+  CommitOrder(ctx context.Context, order entity.Order) error
+  RollbackOrder(ctx context.Context, order entity.Order) error
+  CreateUser(ctx context.Context, balance entity.Balance) error
   Increase(ctx context.Context, balance entity.Balance) error
   GetHistory(ctx context.Context, history entity.History) (entity.History, error)
   GetReport(ctx context.Context, year, month int) (entity.Report, error)
+}
+
+// ReportFile interface serves for saving reports as files
+type ReportFile interface {
+  Create(ctx context.Context, name string, report entity.Report) (string, error)
 }
