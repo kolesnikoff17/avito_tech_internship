8a92a999b2b8355c63b013e0b1e90ace0a1e1fc6
kolesnikoff17 <vladkolesnikofff@gmail.com>
2022-10-22 16:12:47 +0300

    added implementation of ReportFile in csv

---
+++ internal/usecase/report/report_csv.go
@@ -0,0 +1,47 @@
+package report
+
+import (
+	"balance_api/internal/entity"
+	"context"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// BalanceReport keeps a report dir name
+type BalanceReport struct {
+	reportDir string
+}
+
+// New creates new dir and constructs BalanceReport
+func New(d string) *BalanceReport {
+	err := os.Mkdir(strings.Trim(d, "/"), 0750)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &BalanceReport{
+		reportDir: d,
+	}
+}
+
+// Create writes entity.Report to a csv file
+func (r *BalanceReport) Create(ctx context.Context, name string, report entity.Report) (string, error) {
+	name = name + ".csv"
+	file, err := os.Create(r.reportDir + name)
+	if err != nil {
+		return "", fmt.Errorf("ReportFile - Create: %w", err)
+	}
+	defer file.Close()
+	w := csv.NewWriter(file)
+	for _, v := range report.Sums {
+		line := []string{v.Name, v.Sum}
+		err = w.Write(line)
+		if err != nil {
+			return "", fmt.Errorf("ReportFile - Create: %w", err)
+		}
+		w.Flush()
+	}
+	return name, nil
+}
