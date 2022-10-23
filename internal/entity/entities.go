13bffd3d25d5a50adcb1f1c88b3bdd3d8d59ead2
kolesnikoff17 <vladkolesnikofff@gmail.com>
2022-10-23 17:06:37 +0300

    changed field order in order struct

--- internal/entity/entities.go
+++ internal/entity/entities.go
@@ -11,12 +11,12 @@ type Balance struct {
 // Order -.
 type Order struct {
 	ID          int       `json:"-" db:"order_id"`
+	UserID      int       `json:"-" db:"user_id"`
+	Sum         string    `json:"sum" db:"order_sum"`
 	ServiceID   int       `json:"-" db:"service_id"`
 	ServiceName string    `json:"service" db:"service_name"`
-	UserID      int       `json:"-" db:"user_id"`
 	StatusID    int       `json:"-" db:"status_id"`
 	Status      string    `json:"status" db:"status_name"`
-	Sum         string    `json:"sum" db:"order_sum"`
 	Time        time.Time `json:"time" db:"created"`
 }
 
