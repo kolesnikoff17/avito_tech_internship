75ab57da74c1b3098dc40465128e0a8af494d371
kolesnikoff17 <vladkolesnikofff@gmail.com>
2022-10-23 19:44:03 +0300

    added placeholder comments for errors

--- internal/entity/errors.go
+++ internal/entity/errors.go
@@ -3,13 +3,30 @@ package entity
 import "errors"
 
 var (
-	ErrNoID             = errors.New("no such id")
-	ErrOrderExists      = errors.New("order already exists")
-	ErrOrderNoExists    = errors.New("no such order id")
-	ErrNotEnoughMoney   = errors.New("not enough money")
-	ErrOrderMismatch    = errors.New("order data doesnt match order data from db")
+	// ErrNoID -.
+	ErrNoID = errors.New("no such id")
+
+	// ErrOrderExists -.
+	ErrOrderExists = errors.New("order already exists")
+
+	// ErrOrderNoExists -.
+	ErrOrderNoExists = errors.New("no such order id")
+
+	// ErrNotEnoughMoney -.
+	ErrNotEnoughMoney = errors.New("not enough money")
+
+	// ErrOrderMismatch -.
+	ErrOrderMismatch = errors.New("order data doesnt match order data from db")
+
+	// ErrCantChangeStatus -.
 	ErrCantChangeStatus = errors.New("cant update status of committed/canceled order")
-	ErrEmptyReport      = errors.New("no any operations in this month")
-	ErrEmptyPage        = errors.New("this page is empty")
-	ErrNoService        = errors.New("no such service")
+
+	// ErrEmptyReport -.
+	ErrEmptyReport = errors.New("no any operations in this month")
+
+	// ErrEmptyPage -.
+	ErrEmptyPage = errors.New("this page is empty")
+
+	// ErrNoService -.
+	ErrNoService = errors.New("no such service")
 )
