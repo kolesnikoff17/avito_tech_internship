719fbd34fcb827d16192da2a42e5cee856d1b317
kolesnikoff17 <vladkolesnikofff@gmail.com>
2022-10-24 15:55:29 +0300

    added more fancy time output in history request

--- internal/entity/entities.go
+++ internal/entity/entities.go
@@ -1,6 +1,10 @@
 package entity
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
 
 // Balance -.
 type Balance struct {
@@ -10,14 +14,36 @@ type Balance struct {
 
 // Order -.
 type Order struct {
-	ID          int       `json:"-" db:"order_id"`
-	UserID      int       `json:"-" db:"user_id"`
-	Sum         string    `json:"sum" db:"order_sum"`
-	ServiceID   int       `json:"-" db:"service_id"`
-	ServiceName string    `json:"service" db:"service_name"`
-	StatusID    int       `json:"-" db:"status_id"`
-	Status      string    `json:"status" db:"status_name"`
-	Time        time.Time `json:"time" db:"created"`
+	ID          int    `json:"-" db:"order_id"`
+	UserID      int    `json:"-" db:"user_id"`
+	Sum         string `json:"sum" db:"order_sum"`
+	ServiceID   int    `json:"-" db:"service_id"`
+	ServiceName string `json:"service" db:"service_name"`
+	StatusID    int    `json:"-" db:"status_id"`
+	Status      string `json:"status" db:"status_name"`
+	Time        MyTime `json:"time" db:"created"`
+}
+
+// MyTime is more fancy time format in history response
+type MyTime struct {
+	Time time.Time
+}
+
+// MarshalJSON -.
+func (t *MyTime) MarshalJSON() ([]byte, error) {
+	ts := fmt.Sprintf("\"%s\"", t.Time.Format("15:04 02 Jan 06 MST"))
+	return []byte(ts), nil
+}
+
+// Scan -.
+func (t *MyTime) Scan(src interface{}) error {
+	switch src.(type) {
+	case time.Time:
+		t.Time = src.(time.Time)
+	default:
+		return errors.New("scan error: unknown type")
+	}
+	return nil
 }
 
 // History -.
